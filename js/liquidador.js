2a686f362e6d580eef4383603ddae65c894b2b20
Lucia Moreno <marialuciams89@gmail.com>
2020-03-25 09:11:09 -0300

    Initial commit

---
+++ js/liquidador.js
@@ -0,0 +1,10 @@
+let sueldoBasico = document.getElementById('sueldo-basico').value
+let jubilacion = document.getElementById('jubilacion').value
+
+
+console.log('Hola')
+
+function calculoSueldoNeto(){
+  sueldoBasico - jubilacion == sueldoNeto
+  console.log(sueldoNeto)
+}
